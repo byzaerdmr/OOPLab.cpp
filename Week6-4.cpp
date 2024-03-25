f3c3a9bb2d7beec98cb921f1e6a146ed5d4d4a83
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 11:56:15 +0300

    Update Week6-4.cpp

--- Week6-4.cpp
+++ Week6-4.cpp
@@ -2,30 +2,56 @@
 #include <cstring>
 using namespace std;
 
-class prn_obj{
-    int rno;
-    char *name;
+class inventory{
+    char item[20];
+    double cost;  
+    int on_hand;
     public:
-    void set_data(char *n, int r)
-    {  
-        name = n;
-        rno = r;
+    inventory(char *i, double c, int o){
+        strcpy(item, i);
+        cost = c;
+        on_hand = o;
     }
-    void print()
-    {
-        cout<< this-> name <<" has invoked print() funciton"<< endl;
-        cout<< "The roll number is "<< this->rno<< endl;
-    }
+    void show();
 };
 
-int main(){
-    prn_obj ob1, ob2, ob3;
-    ob1.set_data("Suba", 1);
-    ob2.set_data("Kayal", 2);
-    ob3.set_data("Jeysree", 3);
-    ob1.print();
-    ob2.print();
-    ob3.print();
-    return 0;
+void inventory::show(){
+  cout << this->item << ": $" << this->cost <<" On hand: " << this->on_hand << endl;
 }
-  
+
+int main(){
+  inventory ob("Wrench", 4.95, 4);
+  ob.show();
+  return 0;
+}
+
+
+
+//İki kod arasındaki fark, inventory sınıfının tanımlandığı şekilde ve nasıl kullanıldığındadır.
+
+/*#include <iostream>
+#include <cstring>
+using namespace std;
+
+class inventory{
+    char item[20];
+    double cost;  
+    int on_hand;
+    public:
+    inventory(char *i, double c, int o){
+        strcpy(this -> item, i);
+        this -> cost = c;
+        this -> on_hand = o;
+    }
+    void show();
+};
+
+void inventory::show(){
+  cout << this->item << ": $" << this->cost <<" On hand: " << this->on_hand << endl;
+}
+
+int main(){
+  inventory ob("Wrench", 4.95, 4);
+  ob.show();
+  return 0;
+}*/
