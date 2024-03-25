624b8f7f12bd0ee3de3043ff718e44117e01d57f
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 11:40:02 +0300

    Update Week6-5.cpp

--- Week6-5.cpp
+++ Week6-5.cpp
@@ -1,1 +1,25 @@
+//dinamik bellek tahsis ederek bir nesne dizisi oluşturur, bu dizinin her öğesini bir çarpım sonucuyla doldurur ve ardından dinamik belleği serbest bırakır.
 
+#include <iostream>
+using namespace std;
+
+class samp{
+    int i, j;
+    public:
+    void set(int a, int b) {i = a; j = b; }
+    ~samp() { cout << "Destructing" << endl; }
+    int get() {return i*j; }
+};
+
+int main(){
+  samp *p;
+  int i;
+  p = new samp[10];
+  if(!p) {cout << "Allocation error" << endl; return 1; }
+  for (i = 0; i < 10; i++) { p[i].set(i, i); }
+  for (i = 0; i < 10; i++) { cout << "Product [" << i << "]: " << p[i].get() << endl; }
+
+  delete[] p;
+  return 0;
+}
+  
