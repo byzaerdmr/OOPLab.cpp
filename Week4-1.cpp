df576eae17c72e460370b244ec8b3ba850154377
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 12:27:40 +0300

    Update Week4-1.cpp

--- Week4-1.cpp
+++ Week4-1.cpp
@@ -1,19 +1,16 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 class myclass{
     int i,j;
     public:
 
     myclass (int a , int b);
-
     void show();
-
 };
 
 myclass::myclass(int a, int b){
 
     cout<<"In constructor\n";
-
     i=a;
     j=b;
 }
