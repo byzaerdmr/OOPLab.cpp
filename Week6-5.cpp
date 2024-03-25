f52fb8547710e0722f854748f4d764c193159efb
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 11:55:53 +0300

    Update Week6-5.cpp

--- Week6-5.cpp
+++ Week6-5.cpp
@@ -1,3 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class prn_obj{
+    int rno;
+    char *name;
+    public:
+    void set_data(char *n, int r)
+    {  
+        name = n;
+        rno = r;
+    }
+    void print()
+    {
+        cout<< this-> name <<" has invoked print() funciton"<< endl;
+        cout<< "The roll number is "<< this->rno<< endl;
+    }
+};
+
+int main(){
+    prn_obj ob1, ob2, ob3;
+    ob1.set_data("Suba", 1);
+    ob2.set_data("Kayal", 2);
+    ob3.set_data("Jeysree", 3);
+    ob1.print();
+    ob2.print();
+    ob3.print();
+    return 0;
+}
+  
+
+
+
+
+
+
 //dinamik bellek tahsis ederek bir nesne dizisi oluşturur, bu dizinin her öğesini bir çarpım sonucuyla doldurur ve ardından dinamik belleği serbest bırakır.
 
 #include <iostream>
