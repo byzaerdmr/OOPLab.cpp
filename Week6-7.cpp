4e66f4dc0fd074349ccfa08cf4a67228ae5882a1
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 12:15:35 +0300

    Update Week6-7.cpp

--- Week6-7.cpp
+++ Week6-7.cpp
@@ -9,7 +9,7 @@ class myclass{
     ~myclass() { cout << "Destructing " << who << "\n"; }
     int id() { return who; }
 };
-
+//pass by value
 void f(myclass o){cout << "Received " << o.id() << "\n" ; }
 
 int main(){
@@ -17,3 +17,22 @@ int main(){
   f(x);
   return 0;
 }
+
+/*
+#include <iostream >
+using namespace std; 
+class myclass{
+    int who;
+    public:
+    myclass(int n){who = n; cout << "Constructing " << who << "\n";}
+    ~myclass() { cout << "Destructing " << who << "\n"; }
+    int id() { return who; }
+};
+// o is passed by reference.
+void f(myclass &o){cout << "Received " << o.id() << "\n";}
+
+int main(){
+    myclass x(1);
+    f(x);
+    return 0;
+}*/
