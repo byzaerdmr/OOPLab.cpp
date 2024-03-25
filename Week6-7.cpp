962d3ef5ca5e0e30faa84ad74e04a2487159d703
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-25 12:12:39 +0300

    Update Week6-7.cpp

--- Week6-7.cpp
+++ Week6-7.cpp
@@ -1,1 +1,19 @@
+// sınıfa ait bir kurucu ve bir yıkıcı fonksiyon ile bir id() fonksiyonu bulunur ve main() fonksiyonunda bir nesne oluşturarak bir fonksiyona geçirir
 
+#include <iostream>
+using namespace std;
+class myclass{
+    int who;
+    public:
+    myclass(int n){ who=n; cout << "Consturcting " << who << "\n";}
+    ~myclass() { cout << "Destructing " << who << "\n"; }
+    int id() { return who; }
+};
+
+void f(myclass o){cout << "Received " << o.id() << "\n" ; }
+
+int main(){
+  myclass x(1);
+  f(x);
+  return 0;
+}
