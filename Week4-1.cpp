6fdc16ceffbe6db2a90b04b231a8c3a6983cb3bb
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-30 23:00:15 +0300

    Update Week4-1.cpp

--- Week4-1.cpp
+++ Week4-1.cpp
@@ -1,35 +1,5 @@
-#include <iostream>
-using namespace std;
-class myclass{
-    int i,j;
-    public:
 
-    myclass (int a , int b);
-    void show();
-};
-
-myclass::myclass(int a, int b){
-
-    cout<<"In constructor\n";
-    i=a;
-    j=b;
-}
-
-void myclass::show(){
-    cout<< i<< '+' <<j << '='<< i+j<< endl;
-    }
-
-int main() {
-    int x,y;
-    cout<<"Enter two numbers: "<< endl;
-    cin>>x>>y;
-
-    myclass ob(x,y);
-    ob.show();
-    return 0;
-}
-/*
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class myclass{
 
@@ -53,3 +23,28 @@ int main(){
   ob.show();
   return 0;
 }*/
+
+#include <iostream >
+using namespace std;
+class myclass{
+	int a;
+	public:
+	myclass(); // constructor
+	~myclass(); // destructor
+	void show();
+};
+myclass::myclass(){
+	cout << "In constructor\n";
+	a = 10;
+}
+myclass::~myclass(){
+	cout << "Destructing...\n";
+}
+void myclass::show(){
+	cout << a << "\n";
+}
+int main(){
+	myclass ob;
+	ob.show();
+	return 0;
+}
