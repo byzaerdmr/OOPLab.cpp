4c6e3dd7ca2e75ff0448cc749250117e766605f5
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-30 23:08:25 +0300

    Update Week4-1.cpp

--- Week4-1.cpp
+++ Week4-1.cpp
@@ -1,4 +1,3 @@
-
 /*#include <iostream>
 using namespace std;
 class myclass{
@@ -48,3 +47,30 @@ int main(){
 	ob.show();
 	return 0;
 }
+
+/*#include <iostream>
+using namespace std;
+class myclass{
+	int i, j;
+	public:
+	myclass(int a, int b); // constructor
+	void show();
+};
+myclass::myclass(int a, int b){
+	cout << "In constructor\n";
+	i = a;
+    j = b;
+}
+void myclass::show(){
+	cout << i << '+' << j << '=' << i+j << endl; 
+}
+int main(){
+    int x, y;
+    cout << "Enter two numbers: " <<endl;
+    cin >> x >> y;
+
+	myclass ob(x, y);
+	ob.show();
+	return 0;
+}
+*/
